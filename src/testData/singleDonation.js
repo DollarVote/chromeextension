bbbeb7a2997767af75227876bbf075894793bb36
vasvenk <vas.venk@berkeley.edu>
2019-05-06 10:24:54 -0700

    Added smaple data

---
+++ src/testData/singleDonation.js
@@ -0,0 +1,12 @@
+const singleDonation = {
+    donations :[["Causes you support", 2647281], ["Causes you don't support", 10946721]],
+    title: "Procter and Gamble",
+    amount: "10946721",
+    causes: {bad: ["Has donated to pro-life causes.",
+            "Has supported pro-second amendment legislation.",
+            "Supports the construction of the Mexican border wall."],
+        good: ["Supports Medicare for All."]},
+    isGood: false
+};
+
+export default singleDonation;
